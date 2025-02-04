e23fb7b841ea9ec75683a5777a5b94e14d74c8ff
AlpTorac <a.torac@hotmail.com>
2025-02-04 21:09:02 +0100

    Add package-info

---
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/jamopp/unittests/mocktests/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains tests that use mock objects to cover the parts of the similarity
+ * checking process, which are not addressed by other tests.
+ */
+package cipm.consistency.fitests.similarity.jamopp.unittests.mocktests;
