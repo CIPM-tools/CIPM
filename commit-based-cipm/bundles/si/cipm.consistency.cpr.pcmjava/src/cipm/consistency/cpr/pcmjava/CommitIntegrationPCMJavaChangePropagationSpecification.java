f93cbb492b4622d217c6a0bda2821244fdda03b4
AlpTorac <a.torac@hotmail.com>
2025-06-03 17:54:39 +0200

    Update ChangePropagationSpecification

--- commit-based-cipm/bundles/si/cipm.consistency.cpr.pcmjava/src/cipm/consistency/cpr/pcmjava/CommitIntegrationPCMJavaChangePropagationSpecification.java
+++ commit-based-cipm/bundles/si/cipm.consistency.cpr.pcmjava/src/cipm/consistency/cpr/pcmjava/CommitIntegrationPCMJavaChangePropagationSpecification.java
@@ -1,24 +1,15 @@
 package cipm.consistency.cpr.pcmjava;
 
-import cipm.consistency.commitintegration.settings.CommitIntegrationSettingsContainer;
-import cipm.consistency.commitintegration.settings.SettingKeys;
 import mir.reactions.all.AllChangePropagationSpecification;
-import tools.vitruv.applications.pcmjava.seffstatements.code2seff.extended.ExtendedJava2PcmMethodBodyChangePreprocessor;
 
 /**
- * Change propagation specification in order to propagate changes on PCM
- * models to Java.
+ * Change propagation specification in order to propagate changes on PCM models
+ * to Java.
  * 
  * @author Alp Torac Genc
  * @author Manar Mazkatli (advisor)
  * @author Martin Armbruster (advisor)
  */
 public class CommitIntegrationPCMJavaChangePropagationSpecification extends AllChangePropagationSpecification {
-	@Override
-	protected void setup() {
-		super.setup();
-		
-		// Use the PCM -> IM rules, since the change propagation is PCM -> Java
-		this.addChangeMainprocessor(new ExtendedJava2PcmMethodBodyChangePreprocessor());
-	}
+
 }
