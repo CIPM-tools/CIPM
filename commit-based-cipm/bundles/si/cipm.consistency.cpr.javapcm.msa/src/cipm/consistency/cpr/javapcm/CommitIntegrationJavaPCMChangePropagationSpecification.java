85e121a397fc38e4d088ce6c9242eea3ca7d2e85
Martin Armbruster <armbrustermartin@googlemail.com>
2025-06-29 13:41:30 +0200

    CPRs for the SEFF reconstruction are (temporarily) added within the usual Java -> PCM CPRs.

--- commit-based-cipm/bundles/si/cipm.consistency.cpr.javapcm.msa/src/cipm/consistency/cpr/javapcm/CommitIntegrationJavaPCMChangePropagationSpecification.java
+++ commit-based-cipm/bundles/si/cipm.consistency.cpr.javapcm.msa/src/cipm/consistency/cpr/javapcm/CommitIntegrationJavaPCMChangePropagationSpecification.java
@@ -1,6 +1,15 @@
 package cipm.consistency.cpr.javapcm;
 
+import cipm.consistency.commitintegration.settings.CommitIntegrationSettingsContainer;
+import cipm.consistency.commitintegration.settings.SettingKeys;
 import mir.reactions.all.AllChangePropagationSpecification;
+import tools.cipm.seff.Java2PcmMethodBodyChangePreprocessor;
+import tools.cipm.seff.extended.ExtendedJava2PcmMethodBodyChangePreprocessor;
+import tools.cipm.seff.finegrained.FineGrainedJava2PcmMethodBodyChangePreprocessor;
+import tools.vitruv.change.atomic.EChange;
+import tools.vitruv.change.correspondence.Correspondence;
+import tools.vitruv.change.correspondence.view.EditableCorrespondenceModelView;
+import tools.vitruv.change.propagation.ResourceAccess;
 
 /**
  * Change propagation specification in order to propagate changes on JaMoPP models to the PCM models.
@@ -10,16 +19,33 @@ import mir.reactions.all.AllChangePropagationSpecification;
  * @author Martin Armbruster
  */
 public class CommitIntegrationJavaPCMChangePropagationSpecification extends AllChangePropagationSpecification {
+	private Java2PcmMethodBodyChangePreprocessor bodyTransformation;
+	
 	@Override
 	protected void setup() {
 		super.setup();
 		// Change propagation specification for changes on method bodies.
 		// TODO: Move this to another place.
-//		if (CommitIntegrationSettingsContainer.getSettingsContainer()
-//				.getPropertyAsBoolean(SettingKeys.PERFORM_FINE_GRAINED_SEFF_RECONSTRUCTION)) {
-//			this.addChangeMainprocessor(new FineGrainedJava2PcmMethodBodyChangePreprocessor());
-//		} else {
-//			this.addChangeMainprocessor(new ExtendedJava2PcmMethodBodyChangePreprocessor());
-//		}
+		if (CommitIntegrationSettingsContainer.getSettingsContainer()
+				.getPropertyAsBoolean(SettingKeys.PERFORM_FINE_GRAINED_SEFF_RECONSTRUCTION)) {
+			this.bodyTransformation = new FineGrainedJava2PcmMethodBodyChangePreprocessor();
+		} else {
+			this.bodyTransformation = new ExtendedJava2PcmMethodBodyChangePreprocessor();
+		}
+	}
+	
+	@Override
+	public boolean doesHandleChange(EChange change,
+			EditableCorrespondenceModelView<Correspondence> correspondenceModel) {
+		return super.doesHandleChange(change, correspondenceModel) || this.bodyTransformation.doesHandleChange(change, correspondenceModel);
+	}
+	
+	@Override
+	public void propagateChange(EChange change, EditableCorrespondenceModelView<Correspondence> correspondenceModel,
+			ResourceAccess resourceAccess) {
+		super.propagateChange(change, correspondenceModel, resourceAccess);
+		if (this.bodyTransformation.doesHandleChange(change, correspondenceModel)) {
+			this.bodyTransformation.propagateChange(change, correspondenceModel, resourceAccess);
+		}
 	}
 }
--- commit-based-cipm/bundles/si/cipm.consistency.cpr.javapcm.teammates/src/cipm/consistency/cpr/javapcm/teammates/TeammatesJavaPCMChangePropagationSpecification.java
+++ commit-based-cipm/bundles/si/cipm.consistency.cpr.javapcm.teammates/src/cipm/consistency/cpr/javapcm/teammates/TeammatesJavaPCMChangePropagationSpecification.java
@@ -2,6 +2,10 @@ package cipm.consistency.cpr.javapcm.teammates;
 
 import cipm.consistency.cpr.javapcm.teammates.seff.TeammatesJava2PcmMethodBodyChangePreprocessor;
 import mir.reactions.allTeammatesRules.AllTeammatesRulesChangePropagationSpecification;
+import tools.vitruv.change.atomic.EChange;
+import tools.vitruv.change.correspondence.Correspondence;
+import tools.vitruv.change.correspondence.view.EditableCorrespondenceModelView;
+import tools.vitruv.change.propagation.ResourceAccess;
 
 /**
  * Change propagation specification in order to propagate changes on JaMoPP models to the PCM models.
@@ -12,11 +16,28 @@ import mir.reactions.allTeammatesRules.AllTeammatesRulesChangePropagationSpecifi
  * @author Martin Armbruster
  */
 public class TeammatesJavaPCMChangePropagationSpecification extends AllTeammatesRulesChangePropagationSpecification {
+	private TeammatesJava2PcmMethodBodyChangePreprocessor bodyTransformation = new TeammatesJava2PcmMethodBodyChangePreprocessor();
+	
 	@Override
 	protected void setup() {
 		super.setup();
 		// Change propagation specification for changes on method bodies.
 		// TODO: Needs to be added somewhere else
 		// this.addChangeMainprocessor(new TeammatesJava2PcmMethodBodyChangePreprocessor());
+	}
+	
+	@Override
+	public boolean doesHandleChange(EChange change,
+			EditableCorrespondenceModelView<Correspondence> correspondenceModel) {
+		return super.doesHandleChange(change, correspondenceModel) || this.bodyTransformation.doesHandleChange(change, correspondenceModel);
+	}
+	
+	@Override
+	public void propagateChange(EChange change, EditableCorrespondenceModelView<Correspondence> correspondenceModel,
+			ResourceAccess resourceAccess) {
+		super.propagateChange(change, correspondenceModel, resourceAccess);
+		if (this.bodyTransformation.doesHandleChange(change, correspondenceModel)) {
+			this.bodyTransformation.propagateChange(change, correspondenceModel, resourceAccess);
+		}
 	}
 }
