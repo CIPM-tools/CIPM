9e7b48f41bc660d521c6a28385f5d08de4e52b1b
AlpTorac <a.torac@hotmail.com>
2025-09-14 14:47:27 +0200

    Track being set up in logging class
    Edit commentary

--- commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/SimilarityTestLogger.java
+++ commit-based-cipm/fi-tests/cipm.consistency.fitests/src/cipm/consistency/fitests/similarity/SimilarityTestLogger.java
@@ -14,8 +14,19 @@ import org.apache.log4j.PatternLayout;
  * @author Alp Torac Genc
  */
 public class SimilarityTestLogger {
+	/**
+	 * The prefix of each CIPM-related logger without the separating dot (".")
+	 */
 	private final static String cipmRootLoggerName = "cipm";
+	/**
+	 * The prefix of each CIPM-related logger with the separating dot (".")
+	 */
 	private final static String cipmLoggerNamePrefix = cipmRootLoggerName + ".";
+
+	/**
+	 * Whether loggers have been set up
+	 */
+	private static boolean areLoggersSetUp = false;
 
 	/**
 	 * @return The Logger with the given name
@@ -24,52 +35,75 @@ public class SimilarityTestLogger {
 		return Logger.getLogger(loggerName);
 	}
 
+	/**
+	 * @return The Logger for the given class
+	 */
 	private static Logger getLoggerFor(Class<?> cls) {
 		return getLoggerFor(cipmLoggerNamePrefix + cls.getSimpleName());
 	}
 
 	/**
-	 * Sets up all loggers that have the {@code "cipm"} prefix in their name.
+	 * Sets up all loggers that have the {@value #cipmLoggerNamePrefix} prefix in
+	 * their name. Calling this method multiple times will have no effect on
+	 * loggers, unless {@code forceSetUp == true}.
+	 * 
+	 * @param forceSetUp Whether logger should be set anew after already being set
+	 *                   previously
 	 */
-	public static void setUpLogger() {
-		/*
-		 * Order of precedence in logging levels:
-		 * 
-		 * OFF > FATAL > ERROR > WARN > INFO > DEBUG > TRACE > ALL
-		 */
+	public static void setUpLogger(boolean forceSetUp) {
+		if (forceSetUp || !areLoggersSetUp) {
+			/*
+			 * Order of precedence in logging levels:
+			 * 
+			 * OFF > FATAL > ERROR > WARN > INFO > DEBUG > TRACE > ALL
+			 */
 
-		Logger logger = getLoggerFor(cipmRootLoggerName);
-		logger.setLevel(Level.DEBUG);
+			Logger logger = getLoggerFor(cipmRootLoggerName);
+			logger.setLevel(Level.DEBUG);
 
-		// logger = Logger.getLogger("jamopp");
-		// logger.setLevel(Level.ALL);
+			// logger = Logger.getLogger("jamopp");
+			// logger.setLevel(Level.ALL);
 
-		// TODO Re-think how logging should work
+			// TODO Re-think how logging should work
 
-		logger = Logger.getRootLogger();
-		logger.setLevel(Level.OFF);
-		logger.removeAllAppenders();
-		ConsoleAppender ap = new ConsoleAppender(new PatternLayout("[%d{DATE}] %-5p: %c - %m%n"),
-				ConsoleAppender.SYSTEM_OUT);
-		logger.addAppender(ap);
+			logger = Logger.getRootLogger();
+			logger.setLevel(Level.OFF);
+			logger.removeAllAppenders();
+			ConsoleAppender ap = new ConsoleAppender(new PatternLayout("[%d{DATE}] %-5p: %c - %m%n"),
+					ConsoleAppender.SYSTEM_OUT);
+			logger.addAppender(ap);
+			areLoggersSetUp = true;
+		}
 	}
 
 	/**
-	 * Logs the given message at {@link Level#DEBUG} level.
+	 * Sets up all loggers, if not already done.
+	 * 
+	 * @see {@link #setUpLogger(boolean)}
+	 */
+	public static void setUpLogger() {
+		setUpLogger(false);
+	}
+
+	/**
+	 * Logs the given message at {@link Level#DEBUG} level, using the logger for the
+	 * given class.
 	 */
 	public static void logDebugMsg(String msg, Class<?> cls) {
 		getLoggerFor(cls).debug(msg);
 	}
 
 	/**
-	 * Logs the given message at {@link Level#INFO} level.
+	 * Logs the given message at {@link Level#INFO} level, using the logger for the
+	 * given class.
 	 */
 	public static void logInfoMsg(String msg, Class<?> cls) {
 		getLoggerFor(cls).info(msg);
 	}
 
 	/**
-	 * Logs the given message at {@link Level#ERROR} level.
+	 * Logs the given message at {@link Level#ERROR} level, using the logger for the
+	 * given class.
 	 */
 	public static void logErrorMsg(String msg, Class<?> cls) {
 		getLoggerFor(cls).error(msg);
@@ -77,7 +111,7 @@ public class SimilarityTestLogger {
 
 	/**
 	 * Logs the given message at the {@link Level} that corresponds to the given
-	 * priority.
+	 * priority, using the logger for the given class.
 	 */
 	public static void logMsg(String msg, int priority, Class<?> cls) {
 		getLoggerFor(cls).log(Level.toLevel(priority), msg);
