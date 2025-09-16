7d9ca1e985f0a4e0836869fa192fe7213a9e7ed2
AlpTorac <a.torac@hotmail.com>
2025-09-16 12:58:51 +0200

    (WIP) Refactor PCM CPR tests

---
+++ commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/pcm/cprunittests/PcmJavaCprInterfaceNameUserInteractionTest.java
@@ -0,0 +1,122 @@
+package cipm.consistency.vsum.test.pcm.cprunittests;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.emftext.language.java.commons.CommonsPackage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.palladiosimulator.pcm.core.entity.EntityPackage;
+import org.palladiosimulator.pcm.repository.Interface;
+import org.palladiosimulator.pcm.repository.Repository;
+import org.palladiosimulator.pcm.repository.RepositoryFactory;
+
+import cipm.consistency.vsum.test.pcm.userinteraction.ConflictResolutionStrategy;
+import cipm.consistency.vsum.test.pcm.userinteraction.DummyNameConflictResolutionStrategy;
+import cipm.consistency.vsum.test.pcm.userinteraction.PcmUserInteractionManager;
+import mir.reactions.dummyPCMJavaUserInteractionCPRs.DummyPCMJavaUserInteractionCPRsChangePropagationSpecification;
+import tools.vitruv.change.propagation.ChangePropagationSpecification;
+
+public class PcmJavaCprInterfaceNameUserInteractionTest extends AbstractPcmJavaCprTest {
+	@Override
+	protected List<ChangePropagationSpecification> getCPRs() {
+		var list = new ArrayList<ChangePropagationSpecification>();
+		list.add(new DummyPCMJavaUserInteractionCPRsChangePropagationSpecification());
+		return list;
+	}
+
+	public void javaInterfaceNameTest(Function<EObject, ConflictResolutionStrategy[]> createdInterfaceToStratFunc,
+			String expectedJavaInterfaceName) {
+		var javaResource = this.getJavaModelResourceFromJavaFacade();
+		// Assertions.assertEquals(0, javaResource.getContents().size());
+
+		final var pcmInterfaceName = PcmCPRTestConstants.userInteractionTestPCMInterfaceName;
+		final var createdInterface = new Interface[1];
+		var originalRepoRes = this.getResourceFromPcmFacade(repositoryFileName);
+
+		var changes = this.getEChangesFor(originalRepoRes, (r) -> {
+			createdInterface[0] = RepositoryFactory.eINSTANCE.createOperationInterface();
+			createdInterface[0].setEntityName(pcmInterfaceName);
+			var rRepoEObj = (Repository) r.getContents().get(0);
+			rRepoEObj.getInterfaces__Repository().add(createdInterface[0]);
+		});
+
+		if (createdInterfaceToStratFunc != null) {
+			for (var s : createdInterfaceToStratFunc.apply(createdInterface[0])) {
+				PcmUserInteractionManager.addConflictResolutionStrategy(s);
+			}
+		}
+
+		this.getPcmVsumFacade().addChanges(changes);
+		var prop = this.getPcmVsumFacade().propagateResource(originalRepoRes);
+		Assertions.assertNull(prop.getException());
+		this.logPropagatedChanges(prop);
+
+		// Ensure that the change was actually applied to PCM
+		var propagatedResource = this.getResourceFromPcmFacade(repositoryFileName);
+		Assertions.assertEquals(1, propagatedResource.getContents().size());
+		var propagatedRepoEObj = propagatedResource.getContents().get(0);
+		Assertions.assertEquals(1, propagatedRepoEObj.eContents().size());
+		var propagatedRepoInterface = propagatedRepoEObj.eContents().get(0);
+		PcmCprAssertions.assertFeatureValueEquals(propagatedRepoInterface,
+				EntityPackage.Literals.NAMED_ELEMENT__ENTITY_NAME, pcmInterfaceName);
+
+		// Ensure that the Resource is saved after changes are applied
+		var res = this.loadNewResourceInstance(propagatedResource);
+
+		// Ensure that the loaded Resource has the expected contents
+		Assertions.assertEquals(1, res.getContents().size());
+		var resRepoEObj = res.getContents().get(0);
+		Assertions.assertEquals(1, resRepoEObj.eContents().size());
+		var resRepoInterface = resRepoEObj.eContents().get(0);
+		PcmCprAssertions.assertFeatureValueEquals(resRepoInterface, EntityPackage.Literals.NAMED_ELEMENT__ENTITY_NAME,
+				pcmInterfaceName);
+		Assertions.assertTrue(EcoreUtil.equals(resRepoEObj, propagatedRepoEObj));
+		Assertions.assertTrue(EcoreUtil.equals(resRepoInterface, propagatedRepoInterface));
+
+		// Ensure that consequential changes to Java are done too
+		this.removePlaceholderInJavaModelResource();
+		Assertions.assertEquals(1, javaResource.getContents().size());
+		var javaInterface = javaResource.getContents().get(0);
+
+		if (expectedJavaInterfaceName == null) {
+			PcmCprAssertions.assertActualFeatureValueAndPcmManagerConsistent(createdInterface[0], javaInterface,
+					CommonsPackage.Literals.NAMED_ELEMENT__NAME);
+		} else {
+			PcmCprAssertions.assertFeatureValueInPcmManagerEquals(createdInterface[0],
+					CommonsPackage.Literals.NAMED_ELEMENT__NAME, expectedJavaInterfaceName);
+		}
+
+		// Ensure that correspondences are persistent
+		var persistedPcmI = this.getResourceFromPcmFacade(repositoryFileName).getContents().get(0).eContents().get(0);
+		var persistedJavaI = this.getJavaModelResourceFromJavaFacade().getContents().get(0);
+		Assertions.assertEquals(1,
+				this.getPcmVsumFacade().getCorrespondenceView().getCorrespondingEObjects(persistedPcmI).size());
+		var javaCorrespondent = this.getPcmVsumFacade().getCorrespondenceView().getCorrespondingEObjects(persistedPcmI)
+				.iterator().next();
+		Assertions.assertEquals(persistedJavaI, javaCorrespondent);
+		Assertions.assertEquals(1,
+				this.getPcmVsumFacade().getCorrespondenceView().getCorrespondingEObjects(persistedJavaI).size());
+		var pcmCorrespondent = this.getPcmVsumFacade().getCorrespondenceView().getCorrespondingEObjects(persistedJavaI)
+				.iterator().next();
+		Assertions.assertEquals(persistedPcmI, pcmCorrespondent);
+	}
+
+	@Test
+	public void testJavaPCMUserInteraction_InterfaceName_Intercepted() {
+		this.javaInterfaceNameTest((te) -> {
+			return new ConflictResolutionStrategy[] { new DummyNameConflictResolutionStrategy(te,
+					PcmCPRTestConstants.userInteractionTestJavaInterfaceName) };
+		}, PcmCPRTestConstants.userInteractionTestJavaInterfaceName);
+	}
+
+	@Disabled("Enable if manual user interaction is to be tested")
+	@Test
+	public void testJavaPCMUserInteraction_InterfaceName_Manual() {
+		this.javaInterfaceNameTest(null, null);
+	}
+}
