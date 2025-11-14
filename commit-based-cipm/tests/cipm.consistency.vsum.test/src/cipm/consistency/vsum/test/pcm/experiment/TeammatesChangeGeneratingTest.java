9f894b6e7ca2b989c380d3f7fab0d35bc80e6e1b
AlpTorac <a.torac@hotmail.com>
2025-11-14 21:01:29 +0100

    Disable change generation test
    to stop accidentally corrupting vsum target folder

--- commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/pcm/experiment/TeammatesChangeGeneratingTest.java
+++ commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/pcm/experiment/TeammatesChangeGeneratingTest.java
@@ -2,6 +2,8 @@ package cipm.consistency.vsum.test.pcm.experiment;
 
 import java.nio.file.Path;
 
+import org.junit.jupiter.api.Disabled;
+
 import cipm.consistency.vsum.Propagation;
 import cipm.consistency.vsum.test.java.TEAMMATESCITestController;
 import cipm.consistency.vsum.test.pcm.ChangeSaver;
@@ -10,6 +12,7 @@ import cipm.consistency.vsum.test.pcm.ChangeSaver;
  * Evaluates the Java -> PCM change propagation, since it extends
  * TEAMMATESCITestController
  */
+@Disabled("Enable to generate propagated changes during vsum tests")
 public class TeammatesChangeGeneratingTest extends TEAMMATESCITestController {
 	@Override
 	protected void saveChanges(Propagation prop, Path rootDirPath) {
