bdacf138866dd41b18d8dd8c50cf018cf14ac8cd
AlpTorac <a.torac@hotmail.com>
2025-11-14 20:58:51 +0100

    Add method to retrieve Object class

--- commit-based-cipm/bundles/si/cipm.consistency.cpr.pcmjava/src/cipm/consistency/cpr/pcmjava/JavaModelAccess.java
+++ commit-based-cipm/bundles/si/cipm.consistency.cpr.pcmjava/src/cipm/consistency/cpr/pcmjava/JavaModelAccess.java
@@ -78,6 +78,12 @@ public final class JavaModelAccess {
 		return clsSet;
 	}
 
+	public static ConcreteClassifier getObjectClass() {
+		var potentialMatches = findPotentialConcreteClassifiers(Object.class.getSimpleName());
+		return potentialMatches.stream().filter((cls) -> Object.class.getName().equals(cls.getQualifiedName()))
+				.findFirst().orElseGet(() -> null);
+	}
+
 	/**
 	 * Unloads the current Java code model, if it is loaded.
 	 */
