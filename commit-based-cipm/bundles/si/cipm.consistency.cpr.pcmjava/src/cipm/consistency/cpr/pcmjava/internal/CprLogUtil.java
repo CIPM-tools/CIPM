4a89823a46f4b03f5dc9d717776f4c26194dc720
AlpTorac <a.torac@hotmail.com>
2025-12-08 12:00:14 +0100

    Extract PCM CPR logger name

--- commit-based-cipm/bundles/si/cipm.consistency.cpr.pcmjava/src/cipm/consistency/cpr/pcmjava/internal/CprLogUtil.java
+++ commit-based-cipm/bundles/si/cipm.consistency.cpr.pcmjava/src/cipm/consistency/cpr/pcmjava/internal/CprLogUtil.java
@@ -3,7 +3,9 @@ package cipm.consistency.cpr.pcmjava.internal;
 import org.apache.log4j.Logger;
 
 public class CprLogUtil {
+	private static final String loggerName = "cipm.consistency.cpr.pcmjava";
+
 	public static void logCpr(Object cprObj) {
-		Logger.getLogger("cipm.consistency.cpr.pcmjava").info(cprObj.getClass().getSimpleName());
+		Logger.getLogger(loggerName).info(cprObj.getClass().getSimpleName());
 	}
 }
