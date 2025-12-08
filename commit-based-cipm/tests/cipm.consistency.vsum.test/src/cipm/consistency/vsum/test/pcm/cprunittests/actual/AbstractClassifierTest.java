55137c04b5de8a06869bf8138d203c6586353359
AlpTorac <a.torac@hotmail.com>
2025-12-08 15:32:03 +0100

    Add commentary

--- commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/pcm/cprunittests/actual/AbstractClassifierTest.java
+++ commit-based-cipm/tests/cipm.consistency.vsum.test/src/cipm/consistency/vsum/test/pcm/cprunittests/actual/AbstractClassifierTest.java
@@ -10,15 +10,33 @@ import cipm.consistency.cpr.pcmjava.JavaModelAccess;
 import cipm.consistency.cpr.pcmjava.PcmJavaCPRUtils;
 import cipm.consistency.vsum.test.pcm.cprunittests.AbstractPcmJavaCprTest;
 
+/**
+ * An abstract test class for PCM to Java CPRs that contains utility methods for
+ * constructing and adding Java {@link Classifier}s into Java code models.
+ * 
+ * @author Alp Torac Genc
+ */
 public abstract class AbstractClassifierTest extends AbstractPcmJavaCprTest {
 	protected static final String namespaceSeparatorRegex = "\\.";
 
+	/**
+	 * Saves and reloads the Java code model used by the VSUM. Sets the Java model
+	 * stored in {@link JavaModelAccess} as the reloaded Java model.
+	 */
 	protected void saveAndReloadJavaModelResource() {
 		JavaModelAccess.saveJavaModel();
 		this.getJavaFacade().reload();
 		JavaModelAccess.setJavaModel(this.getJavaFacade().getResource());
 	}
 
+	/**
+	 * Creates and adds an Interface to the given Resource.
+	 * 
+	 * @param modelRes    Resource instance where the Interface should be inserted
+	 * @param javaIfcName Interface name
+	 * @param javaIfcNss  Interface namespaces
+	 * @return The created Interface
+	 */
 	protected org.emftext.language.java.classifiers.Interface addInterfaceToJavaModelResource(Resource modelRes,
 			String javaIfcName, List<String> javaIfcNss) {
 		var ifc = ClassifiersFactory.eINSTANCE.createInterface();
@@ -27,6 +45,14 @@ public abstract class AbstractClassifierTest extends AbstractPcmJavaCprTest {
 		return ifc;
 	}
 
+	/**
+	 * Creates and adds a Class to the given Resource.
+	 * 
+	 * @param modelRes  Resource instance where the Class should be inserted
+	 * @param className Class name
+	 * @param classNss  Class namespaces
+	 * @return The created Class
+	 */
 	protected org.emftext.language.java.classifiers.Class addClassToJavaModelResource(Resource modelRes,
 			String className, List<String> classNss) {
 		var cls = ClassifiersFactory.eINSTANCE.createClass();
@@ -35,6 +61,13 @@ public abstract class AbstractClassifierTest extends AbstractPcmJavaCprTest {
 		return cls;
 	}
 
+	/**
+	 * Creates and adds a Package to the given Resource.
+	 * 
+	 * @param modelRes Resource instance where the Package should be inserted
+	 * @param pacNss   Package namespaces
+	 * @return The created Package
+	 */
 	protected org.emftext.language.java.containers.Package addPackageToJavaModelResource(Resource modelRes,
 			List<String> pacNss) {
 		var pac = ContainersFactory.eINSTANCE.createPackage();
@@ -43,6 +76,13 @@ public abstract class AbstractClassifierTest extends AbstractPcmJavaCprTest {
 		return pac;
 	}
 
+	/**
+	 * Creates and adds a Module to the given Resource.
+	 * 
+	 * @param modelRes Resource instance where the Package should be inserted
+	 * @param modNss   Module namespaces
+	 * @return The created Module
+	 */
 	protected org.emftext.language.java.containers.Module addModuleToJavaModelResource(Resource modelRes,
 			List<String> modNss) {
 		var mod = ContainersFactory.eINSTANCE.createModule();
