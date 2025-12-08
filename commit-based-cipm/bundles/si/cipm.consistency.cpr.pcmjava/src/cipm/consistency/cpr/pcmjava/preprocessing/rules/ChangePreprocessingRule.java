ec2befbee82f4ede52c81af3630c459f3c2b6292
AlpTorac <a.torac@hotmail.com>
2025-12-08 10:42:50 +0100

    (WIP) Remove unused classes, methods and imports

--- commit-based-cipm/bundles/si/cipm.consistency.cpr.pcmjava/src/cipm/consistency/cpr/pcmjava/preprocessing/rules/ChangePreprocessingRule.java
+++
@@ -1,9 +0,0 @@
-package cipm.consistency.cpr.pcmjava.preprocessing.rules;
-
-import java.util.List;
-
-import tools.vitruv.change.atomic.EChange;
-
-public abstract class ChangePreprocessingRule {
-	public abstract List<EChange> apply(List<EChange> changeSequence);
-}
