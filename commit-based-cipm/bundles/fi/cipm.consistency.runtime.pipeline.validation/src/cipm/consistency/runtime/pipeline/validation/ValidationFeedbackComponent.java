68410a751fcfc47142543f8b3eb424eb5fe49129
Martin Armbruster <armbrustermartin@googlemail.com>
2026-01-12 12:06:26 +0100

    Combined all adjusted parts in the ValidationFeedbackComponent.

--- commit-based-cipm/bundles/fi/cipm.consistency.runtime.pipeline.validation/src/cipm/consistency/runtime/pipeline/validation/ValidationFeedbackComponent.java
+++ commit-based-cipm/bundles/fi/cipm.consistency.runtime.pipeline.validation/src/cipm/consistency/runtime/pipeline/validation/ValidationFeedbackComponent.java
@@ -4,49 +4,61 @@ import java.util.List;
 
 import org.pcm.headless.shared.data.results.InMemoryResultRepository;
 
-import cipm.consistency.base.core.health.AbstractHealthStateComponent;
-import cipm.consistency.base.core.health.HealthState;
-import cipm.consistency.base.core.health.HealthStateObservedComponent;
 import cipm.consistency.base.shared.pcm.InMemoryPCM;
 import cipm.consistency.bridge.monitoring.records.PCMContextRecord;
 import cipm.consistency.runtime.pipeline.validation.data.ValidationData;
 import cipm.consistency.runtime.pipeline.validation.data.ValidationMetricValue;
 import cipm.consistency.runtime.pipeline.validation.data.metric.IValidationMetric;
+import cipm.consistency.runtime.pipeline.validation.data.metric.impl.ServiceCallAverageDistanceAbsolute;
+import cipm.consistency.runtime.pipeline.validation.data.metric.impl.ServiceCallAverageDistanceRelative;
+import cipm.consistency.runtime.pipeline.validation.data.metric.impl.ServiceCallKSTestMetric;
+import cipm.consistency.runtime.pipeline.validation.data.metric.impl.ServiceCallMedianDistanceAbsolute;
+import cipm.consistency.runtime.pipeline.validation.data.metric.impl.ServiceCallQ1DistanceAbsolute;
+import cipm.consistency.runtime.pipeline.validation.data.metric.impl.ServiceCallQ3DistanceAbsolute;
+import cipm.consistency.runtime.pipeline.validation.data.metric.impl.ServiceCallWassersteinDistanceMetric;
 import cipm.consistency.runtime.pipeline.validation.eval.ValidationDataExtractor;
 import cipm.consistency.runtime.pipeline.validation.simulation.HeadlessPCMSimulator;
 
-public class ValidationFeedbackComponent extends AbstractHealthStateComponent implements IValidationProcessor {
-	private HeadlessPCMSimulator simulator;
+public class ValidationFeedbackComponent implements IValidationProcessor {
+	private HeadlessPCMSimulator simulator = new HeadlessPCMSimulator();
 
-	private ValidationDataExtractor extractor;
+	private ValidationDataExtractor extractor = new ValidationDataExtractor();
 
-	private List<IValidationMetric<?>> metrics;
+	private List<IValidationMetric<?>> metrics = List.of(
+			new ServiceCallAverageDistanceAbsolute(),
+			new ServiceCallAverageDistanceRelative(),
+			new ServiceCallKSTestMetric(),
+			new ServiceCallMedianDistanceAbsolute(),
+			new ServiceCallQ1DistanceAbsolute(),
+			new ServiceCallQ3DistanceAbsolute(),
+			new ServiceCallWassersteinDistanceMetric()
+		);
 
 	private boolean workingBefore = false;
 	private boolean initial = true;
-
-	public ValidationFeedbackComponent() {
-		super(HealthStateObservedComponent.VALIDATION_CONTROLLER, HealthStateObservedComponent.CONFIGURATION);
+	
+	public ValidationFeedbackComponent() throws Exception {
+		simulator.initialize();
 	}
 
-	@Scheduled(fixedRate = 10000L, initialDelay = 0)
-	public void checkAvailability() {
-		if (checkPreconditions()) {
-			boolean reachable = simulator.isReachable();
-			if (reachable && !workingBefore) {
-				super.removeAllProblems();
-				super.updateState();
-				workingBefore = true;
-				initial = false;
-			} else if (!reachable && (workingBefore || initial)) {
-				super.reportError(
-						"Headless PCM simulator is not reachable. Please check your configuration and/or the availability of the headless simulator.");
-				super.updateState();
-				workingBefore = false;
-				initial = false;
-			}
-		}
-	}
+	// @Scheduled(fixedRate = 10000L, initialDelay = 0)
+//	public void checkAvailability() {
+//		if (checkPreconditions()) {
+//			boolean reachable = simulator.isReachable();
+//			if (reachable && !workingBefore) {
+//				super.removeAllProblems();
+//				super.updateState();
+//				workingBefore = true;
+//				initial = false;
+//			} else if (!reachable && (workingBefore || initial)) {
+//				super.reportError(
+//						"Headless PCM simulator is not reachable. Please check your configuration and/or the availability of the headless simulator.");
+//				super.updateState();
+//				workingBefore = false;
+//				initial = false;
+//			}
+//		}
+//	}
 
 	@Override
 	public ValidationData process(InMemoryPCM instance, List<PCMContextRecord> monitoringData, String taskName) {
@@ -76,10 +88,5 @@ public class ValidationFeedbackComponent extends AbstractHealthStateComponent im
 	@Override
 	public void clearSimulationData() {
 		simulator.clearAllSimulationData();
-	}
-
-	@Override
-	protected void onMessage(HealthStateObservedComponent source, HealthState state) {
-		// nothing to do here because no deps
 	}
 }
